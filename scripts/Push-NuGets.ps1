993a86fb4ed5dd0524f6d80a77820f4920bd5729
Ricardo Carneiro <rjorge.carneiro@gmail.com>
2023-09-16 16:31:15 +0100

    Small clean up

---
+++ scripts/Push-NuGets.ps1
@@ -0,0 +1,17 @@
+param (
+    [string]$ApiKey = $(throw "-Key is required."),
+    [string]$Path = ".\dist\NuGets",
+    [string]$Source = "https://api.nuget.org/v3/index.json"
+)
+
+$NuGetFiles = Get-ChildItem -Path $Path -Filter *.nupkg -Name
+
+Write-Host "-----------------"
+Write-Host "About to push $(($NuGetFiles | Measure-Object).Count) NuGet packages into $Source"
+Write-Host "-----------------"
+
+foreach ($NuGet in $NuGetFiles)
+{ 
+    Write-Host "Pushing $NuGet";
+    nuget push $Path/$NuGet -ApiKey $ApiKey -Source $Source
+}
