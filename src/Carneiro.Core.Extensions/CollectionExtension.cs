c360c4a76747d1fe28d4510113a09535262a0fa3
Ricardo Carneiro <rjorge.carneiro@gmail.com>
2024-02-29 20:20:38 +0000

    Remove and sort usings

--- src/Carneiro.Core.Extensions/CollectionExtension.cs
+++ src/Carneiro.Core.Extensions/CollectionExtension.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace Carneiro.Core.Extensions;
+﻿namespace Carneiro.Core.Extensions;
 
 /// <summary>
 /// Extensions for collections
