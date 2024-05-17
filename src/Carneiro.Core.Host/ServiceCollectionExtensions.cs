80fb1e0afbdae88176834222ee21397a4a0e1662
Ricardo Carneiro <rjorge.carneiro@gmail.com>
2024-05-17 09:03:12 +0100

    Minor upgrades on host

---
+++ src/Carneiro.Core.Host/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Carneiro.Core.Host;
+
+/// <summary>
+/// Extensions for <see cref="IServiceCollection"/>.
+/// </summary>
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// Adds <see cref="WorkerService"/> as <see cref="IHostedService"/>.
+    /// </summary>
+    /// <param name="services"></param>
+    public static IServiceCollection AddWorkerService(this IServiceCollection services) => services.AddHostedService<WorkerService>();
+}
