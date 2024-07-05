a70da4cc3d18ab19f2d00910d6a08587d0178d05
Ricardo Carneiro <rjorge.carneiro@gmail.com>
2024-07-05 08:53:44 +0100

    Add new store procedure execution

---
+++ src/Carneiro.Core.Repository.Abstractions/StoreProcedureResult.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace Carneiro.Core.Repository.Abstractions;
+
+/// <summary>
+/// Class that represents the result of a stored procedure.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class StoreProcedureResult<T> where T : class
+{
+    /// <summary>
+    /// Gets the result.
+    /// </summary>
+    public T Result { get; init; }
+
+    /// <summary>
+    /// Gets the output.
+    /// </summary>
+    public Dictionary<string, object> Output { get; init; }
+
+    /// <inheritdoc />
+    public override string ToString()
+    {
+        var sb = new StringBuilder($"Output = {Output.Count}");
+
+        foreach (KeyValuePair<string, object> entry in Output)
+        {
+            sb.AppendLine($"Key = {entry.Key} Value = {entry.Value}");
+        }
+
+        return sb.ToString();
+    }
+}
