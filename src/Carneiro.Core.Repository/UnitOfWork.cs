c77c3743ff20c8f01f0c9ceedd5a90e628581b0f
Ricardo Carneiro <rjorge.carneiro@gmail.com>
2024-07-05 09:12:37 +0100

    Add more overloads

--- src/Carneiro.Core.Repository/UnitOfWork.cs
+++ src/Carneiro.Core.Repository/UnitOfWork.cs
@@ -232,6 +232,65 @@ public class UnitOfWork<TDbContext> : IUnitOfWork where TDbContext : DbContext
     public virtual Task SaveAsync(CancellationToken cancellationToken) => DbContext.SaveChangesAsync(cancellationToken);
 
     /// <inheritdoc />
+    public virtual Task<T> ExecuteStoredProcedureAsync<T>(string sql, Func<DbDataReader, Task<T>> action) where T : class =>
+        ExecuteStoredProcedureAsync(sql, sqlParameters: new Dictionary<string, object>(), CommandBehavior.Default, action, CancellationToken.None);
+
+    /// <inheritdoc />
+    public Task<T> ExecuteStoredProcedureAsync<T>(string sql, Func<DbDataReader, Task<T>> action, CancellationToken cancellationToken) where T : class =>
+        ExecuteStoredProcedureAsync(sql, new Dictionary<string, object>(), CommandBehavior.Default, action, cancellationToken);
+
+    /// <inheritdoc />
+    public Task<T> ExecuteStoredProcedureAsync<T>(string sql, CommandBehavior commandBehavior, Func<DbDataReader, Task<T>> action) where T : class =>
+        ExecuteStoredProcedureAsync(sql, new Dictionary<string, object>(), commandBehavior, action, CancellationToken.None);
+
+    /// <inheritdoc />
+    public Task<T> ExecuteStoredProcedureAsync<T>(string sql, Dictionary<string, object> sqlParameters, Func<DbDataReader, Task<T>> action) where T : class =>
+        ExecuteStoredProcedureAsync(sql, sqlParameters, CommandBehavior.Default, action, CancellationToken.None);
+
+    /// <inheritdoc />
+    public Task<T> ExecuteStoredProcedureAsync<T>(string sql, Dictionary<string, object> sqlParameters, Func<DbDataReader, Task<T>> action, CancellationToken cancellationToken) where T : class =>
+        ExecuteStoredProcedureAsync(sql, sqlParameters, CommandBehavior.Default, action, cancellationToken);
+
+    /// <inheritdoc />
+    public Task<T> ExecuteStoredProcedureAsync<T>(string sql, Dictionary<string, object> sqlParameters, CommandBehavior commandBehavior, Func<DbDataReader, Task<T>> action) where T : class =>
+        ExecuteStoredProcedureAsync(sql, sqlParameters, commandBehavior, action, CancellationToken.None);
+
+    /// <inheritdoc />
+    public virtual async Task<T> ExecuteStoredProcedureAsync<T>(string sql, Dictionary<string, object> sqlParameters, CommandBehavior commandBehavior, Func<DbDataReader, Task<T>> action, CancellationToken cancellationToken)
+        where T : class
+    {
+        Logger.LogInformation("Building new {StoredProcedure} with sql {Sql}", nameof(CommandType.StoredProcedure), sql);
+
+        await using DbConnection connection = DbContext.Database.GetDbConnection();
+        await using DbCommand command = connection.CreateCommand();
+
+        command.CommandText = sql;
+        command.CommandType = CommandType.StoredProcedure;
+
+        if (sqlParameters.Count > 0)
+        {
+            Logger.LogInformation("Adding {SqlParametersCount} parameters to the database command", sqlParameters.Count);
+
+            foreach (KeyValuePair<string, object> sqlParameter in sqlParameters)
+            {
+                DbParameter parameter = command.CreateParameter();
+                parameter.ParameterName = sqlParameter.Key;
+                parameter.Value = sqlParameter.Value;
+
+                Logger.LogInformation("Parameter: {ParameterParameterName} Value: {ParameterValue}", parameter.ParameterName, parameter.Value);
+
+                command.Parameters.Add(parameter);
+            }
+        }
+
+        Logger.LogInformation("Executing sql '{Sql}' as '{CommandCommandType}'", sql, command.CommandType);
+
+        await connection.OpenAsync(cancellationToken);
+        await using DbDataReader dbDataReader = await command.ExecuteReaderAsync(commandBehavior, cancellationToken);
+        return await action(dbDataReader);
+    }
+
+    /// <inheritdoc />
     public virtual async Task<StoreProcedureResult<T>> ExecuteStoredProcedureAsync<S, T>(string sql, IEnumerable<S> sqlParameters, CommandBehavior commandBehavior, Func<DbDataReader, Task<T>> action,
         CancellationToken cancellationToken)
         where S : DbParameter
