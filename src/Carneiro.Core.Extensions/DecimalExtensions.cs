8820a0d1f2cf43536a34a6a888e9b0d9e9f5cc52
Ricardo Carneiro <rjorge.carneiro@gmail.com>
2024-07-28 09:12:05 +0100

    Add more date time extensions

--- src/Carneiro.Core.Extensions/DecimalExtensions.cs
+++ src/Carneiro.Core.Extensions/DecimalExtensions.cs
@@ -9,6 +9,17 @@ public static class DecimalExtensions
     /// Converts a <see cref="decimal"/> into a two decimal places string.
     /// </summary>
     /// <param name="amount">The amount.</param>
-    /// <returns></returns>
     public static string ToQuantityFormat(this decimal amount) => amount.ToString("N2");
+
+    /// <summary>
+    /// Converts a <see cref="decimal"/> into a two decimal places string.
+    /// </summary>
+    /// <param name="decimal"></param>
+    public static string ToPrice(this decimal? @decimal) => @decimal.HasValue ? @decimal.ToPrice() : null;
+
+    /// <summary>
+    /// Converts a <see cref="decimal"/> into a two decimal places string.
+    /// </summary>
+    /// <param name="decimal"></param>
+    public static string ToPrice(this decimal @decimal) => @decimal.ToString("N2");
 }
