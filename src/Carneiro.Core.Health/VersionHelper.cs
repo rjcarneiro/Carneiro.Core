2439b8b1530a87eeeab4e39ab06f54d129bc4015
Ricardo Carneiro <rjorge.carneiro@gmail.com>
2024-09-28 09:14:58 +0100

    Improve version model

--- src/Carneiro.Core.Health/VersionHelper.cs
+++ src/Carneiro.Core.Health/VersionHelper.cs
@@ -10,8 +10,7 @@ public static class VersionHelper
     /// </summary>
     public static string GetSimplerVersion()
     {
-        Assembly assembly = Assembly.GetEntryAssembly() 
-                            ?? Assembly.GetExecutingAssembly();
+        Assembly assembly = GetAssembly();
 
         return assembly.GetName().Version!.ToString();
     }
@@ -21,13 +20,14 @@ public static class VersionHelper
     /// </summary>
     public static VersionModel GetVersion()
     {
-        Assembly assembly = Assembly.GetEntryAssembly() 
-                            ?? Assembly.GetExecutingAssembly();
+        Assembly assembly = GetAssembly();
 
         return new VersionModel
         {
-            Date = File.GetLastWriteTime(string.IsNullOrEmpty(assembly.Location) ? AppContext.BaseDirectory : assembly.Location),
-            Version = assembly.GetName().Version!.ToString()
+            Date = File.GetLastWriteTime(string.IsNullOrEmpty(assembly.Location) ? AppContext.BaseDirectory : assembly.Location).ToUniversalTime(), 
+            Version = assembly.GetName().Version!.ToString(),
         };
     }
+
+    private static Assembly GetAssembly() => Assembly.GetEntryAssembly() ?? Assembly.GetExecutingAssembly();
 }
